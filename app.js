04a0bb6f38817ec662c28b06991d86e960a9019f
xmasnoaa <xmas.noaa@gmail.com>
2022-07-18 16:21:50 -0500

    Content and Functionality update

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const easterEgg = document.querySelector('h1');
+const list = document.querySelector('ul');
+let socials = document.querySelectorAll('svg.ionicon');
 
+//dark mode
 function nightMode() {
   const page = document.querySelector('body');
   page.classList.toggle('dark');
+  easterEgg.classList.toggle('darkh1');
+  socials.forEach((values) => {
+    values.classList.toggle('dark');
+  });
 }
 
 easterEgg.addEventListener('click', nightMode);
 
-const val = document.querySelector('body');
+//social animate
 
-console.log(val);
+function socialOpen() {
+  console.log(list.children[0]);
+  console.log(list.children[1]);
+  console.log(list.children[2]);
+}
+
+list.addEventListener('mousover', socialOpen);
+
+console.log(socials);
