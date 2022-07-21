8ac314c31a3570e3ae7f1e213082ef8f8fa79481
xmasnoaa <xmas.noaa@gmail.com>
2022-07-21 01:33:19 -0500

    scrollbar transition and social media hover link event

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const easterEgg = document.querySelector('h1');
-const list = document.querySelector('ul');
-let socials = document.querySelectorAll('svg.ionicon');
-
+const socials = document.querySelectorAll('svg.ionicon');
+const page = document.querySelector('body');
+const list = document.querySelectorAll('ul#socials li svg');
 //dark mode
 function nightMode() {
-  const page = document.querySelector('body');
   page.classList.toggle('dark');
   easterEgg.classList.toggle('darkh1');
   socials.forEach((values) => {
@@ -12,16 +11,64 @@ function nightMode() {
   });
 }
 
-easterEgg.addEventListener('click', nightMode);
+// scrolling fade in
 
-//social animate
+function reveal() {
+  let reveals = document.querySelectorAll('.reveal');
 
-function socialOpen() {
-  console.log(list.children[0]);
-  console.log(list.children[1]);
-  console.log(list.children[2]);
+  for (let i = 0; reveals.length; i++) {
+    let windowheight = window.innerHeight;
+    let revealtop = reveals[i].getBoundingClientRect().top;
+    const revealpoint = 100;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
 }
 
-list.addEventListener('mousover', socialOpen);
+easterEgg.addEventListener('click', nightMode);
 
-console.log(socials);
+window.addEventListener('scroll', reveal);
+
+// // socials reveal
+console.log(list);
+
+list.forEach(function (item) {
+  item.addEventListener('mouseover', function socialReveal() {
+    console.log(item.classList[1]);
+    //create span Element to place your attribute
+    const spanEl = document.createElement('span');
+
+    let x = item.classList[1];
+    switch (x) {
+      // set your handle text and add it next to the icons
+      case 'instagram':
+        console.log('this is Instagram');
+        //set your handle text
+        spanEl.appendChild(document.createTextNode('xmasnoaa'));
+        item.parentNode.insertBefore(spanEl, item.nextSibling);
+        item.removeEventListener('mouseover', socialReveal);
+
+        break;
+
+      case 'github':
+        console.log('this is github');
+        spanEl.appendChild(document.createTextNode('xmasnoaa'));
+        item.parentNode.insertBefore(spanEl, item.nextSibling);
+        item.removeEventListener('mouseover', socialReveal);
+
+        break;
+
+      case 'linkedin':
+        console.log('this is linkedin');
+        spanEl.appendChild(document.createTextNode('Tam Duong'));
+        item.parentNode.insertBefore(spanEl, item.nextSibling);
+        item.removeEventListener('mouseover', socialReveal);
+
+        break;
+    }
+  });
+});
